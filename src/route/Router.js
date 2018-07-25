d61d7e7a4aed7d6f02b8d4a6d40a85e66ce423b0
ufresh2013 <ufresh2013@163.com>
2018-07-25 17:07:57 +0800

    页面

--- src/route/Router.js
+++ src/route/Router.js
@@ -1,8 +1,10 @@
 import { Switch, Route } from 'react-router-dom';
 import React, { Component } from 'react';
-import Home from '../components/Home';
+import Home from '../page/Home';
 import Roster from '../components/Roster';
 import SelectQuestion from '../page/SelectQuestion';
+import Manage from '../page/Manage';
+import Preview from '../page/Preview';
 // import { deflateRaw } from 'zlib';
 
 
@@ -13,6 +15,8 @@ export default class Main extends Component {
               <Route exact path='/' component={Home}/>
               <Route path='/roster' component={Roster}/>
               <Route path='/question' component={SelectQuestion}/>
+              <Route path='/manage' component={Manage}/>
+              <Route path='/preview' component={Preview}/>
             </Switch>
           </main>)
     }
